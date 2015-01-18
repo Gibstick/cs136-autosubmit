d272b0f95f358b0cd4fd47c0a5a83865702f8921
Charlie <gibsticker@gmail.com>
2015-01-18 11:57:48 -0500

    added documentation and fixed style

--- autosubmit.py
+++ autosubmit.py
@@ -9,61 +9,112 @@ import sys
 # monkey-patch SSL because verification fails on 2.7.9
 if sys.hexversion == 34015728:
     import ssl
+
     if hasattr(ssl, '_create_unverified_context'):
+        # noinspection PyProtectedMember
         ssl._create_default_https_context = ssl._create_unverified_context
 
 RACKET_KEYWORD = ';;;!'
 C_KEYWORD = '///'
 LINE_SEARCH_LIMIT = 10
-langLookup = {'.rkt' : RACKET_KEYWORD, '.c' : C_KEYWORD}
+langLookup = {'.rkt': RACKET_KEYWORD, '.c': C_KEYWORD}
 
 username = raw_input('Username: ')
 password = getpass('Password: ')
 
 m = Marmoset(username, password)
 
+
 class MarmosetAssignment:
-    """Stores one marmoset assignment problem and a list of its files"""
+    """
+    Stores one marmoset assignment problem and a list of its files.
+
+    """
 
     def __init__(self, course='', assignment=''):
+        """
+        Construct with course and assignment.
+
+        :param course:
+        :type course: str
+        :param assignment:
+        :type assignment: str
+
+        :return: An assignment instance
+        :rtype: MarmosetAssignment
+        """
         self.course = course
         self.assignment = assignment
         self.files = []
 
+    def set_course(self, course):
+        """
+        Setter for the instance field "course"
 
-    def setCourse(self, course):
+        :param course:
+        :type course: str
+
+        :return: None
+        """
         self.course = course
 
-    def setAssignment(self, assignment):
+    def set_assignment(self, assignment):
+        """
+        Setter for the instance field "assignment"
+
+        :param assignment:
+        :type assignment: str
+
+        :return: None
+        """
         self.assignment = assignment
 
-    def addFile(self, f):
+    def add_file(self, f):
+        """
+        Add a file to the instance's list of files
+
+        :param f: A path
+        :type f: str
+
+        :return: None
+        """
         self.files.append(f)
 
-    def submit(self, m):
+    def submit(self, marmoset):
+        """
+        Submit all files in the instance's files field.
+
+        :param marmoset: Marmoset instance
+        :type marmoset: Marmoset
+
+        :return: None
+        """
         if len(self.files) == 1:
             print("Submitting " + self.course + " " + self.assignment)
-            self.files = self.files[0] # Fix for zipping the entire directory structure
+            self.files = self.files[0]  # Fix for zipping the entire directory structure
 
-        m.submit(self.course, self.assignment, self.files)
+        marmoset.submit(self.course, self.assignment, self.files)
 
 
-
-def getParamsFromFile(path):
+def get_params_from_file(path):
     """
     Get a list of parameters by searching the file.
 
-    param path: Path to the file (string)
-    returns: A list of parameters [course, assignment]
+    :param path: Path to the file
+    :type path: str
+
+    :return: A list of parameters [course, assignment]
+    :rtype: list
     """
     f = open(path)
-    fileType = os.path.splitext(path)[1]
-    keyword = langLookup[fileType]
+    file_extension = os.path.splitext(path)[1]
+    keyword = langLookup[file_extension]
 
     for line in f:
         if line.startswith(keyword):
             params = line.split(' ', 2)
-            if len(params) != 3: return False
+            if len(params) != 3:
+                return False
 
             params.pop(0)
             params[1] = params[1].rstrip(')\n')
@@ -75,48 +126,57 @@ def getParamsFromFile(path):
     f.close()
     return False
 
-def getFilePaths(filetype):
+
+def get_file_paths(file_extension):
     """
     Get all the files of the specified type from the cwd.
 
-    param filetype: The filetype to search for
-    returns: A list of file paths
+    :param file_extension: The file type to search for
+    :type file_extension: str
+
+    :return: A list of file paths
+    :rtype: str
     """
     cwd = os.getcwd()
-    return glob.glob(cwd + '/*' + filetype)
+    return glob.glob(cwd + '/*' + file_extension)
 
-def getAllParams(fileList):
+
+def get_all_params(file_list):
     """
     Get all parameters from the list of files.
 
-    param files: A list of files
-    returns: a list of MarmosetAssignments
+
+    :param file_list: A list of file paths
+    :type file_list: list
+    :return: A list of MarmosetAssignments
+    :rtype: list
     """
-    marmoProblems = {}
+    marmo_problems = {}
+    params_map = zip(file_list, map(lambda f: get_params_from_file(f), file_list))
 
-    paramsMap = zip(fileList, map(lambda f: getParamsFromFile(f), fileList))
-
-    validFiles = (x for x in paramsMap if x[1] != False)
-    for entry in validFiles:
+    valid_files = (x for x in params_map if x[1] is not False)
+    for entry in valid_files:
         course = entry[1][0]
         assignment = entry[1][1]
-        fileName = entry[0] # FIXME: Zip behaviour is weird
-        id = (course, assignment) # 'course' + 'assignment'
-        if id in marmoProblems:
+        filename = entry[0]  # FIXME: Zip behaviour is weird
+        key = (course, assignment)  # 'course' + 'assignment'
+        if key in marmo_problems:
             # add filename to existing MarmosetAssignment
-            marmoProblems[id].addFile(fileName)
+            marmo_problems[key].add_file(filename)
         else:
-            marmoProblems[id] = MarmosetAssignment(course, assignment)
-            marmoProblems[id].addFile(fileName)
+            marmo_problems[key] = MarmosetAssignment(course, assignment)
+            marmo_problems[key].add_file(filename)
 
-    return marmoProblems.values()
+    return marmo_problems.values()
 
-def submitAll(marmosetAssignments, m):
-    for problem in marmosetAssignments:
-        problem.submit(m)
+
+def submit_all(assignments, marmoset):
+    for problem in assignments:
+        problem.submit(marmoset)
+
 
 files = []
-for filetype in langLookup:
-    files += getFilePaths(filetype)
+for file_exts in langLookup:
+    files += get_file_paths(file_exts)
 
-submitAll(getAllParams(files), m)
+submit_all(get_all_params(files), m)
