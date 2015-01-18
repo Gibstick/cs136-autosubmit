a5642d1f73a356fee1a01146fb1771a2a7355617
Charlie <gibsticker@gmail.com>
2015-01-18 09:37:06 -0500

    added multiple file per assignment support

--- autosubmit.py
+++ autosubmit.py
@@ -6,25 +6,60 @@ import os
 import glob
 import sys
 
-
 # monkey-patch SSL because verification fails on 2.7.9
 if sys.hexversion == 34015728:
     import ssl
     if hasattr(ssl, '_create_unverified_context'):
         ssl._create_default_https_context = ssl._create_unverified_context
 
+RACKET_KEYWORD = ';;;!'
+C_KEYWORD = '///'
+LINE_SEARCH_LIMIT = 10
+langLookup = {'.rkt' : RACKET_KEYWORD, '.c' : C_KEYWORD}
+
 username = raw_input('Username: ')
 password = getpass('Password: ')
 
 m = Marmoset(username, password)
 
-def getParams(path):
-    """Get a list of parameters by searching the file."""
+class MarmosetAssignment:
+    """Stores one marmoset assignment problem and a list of its files"""
+
+    def __init__(self, course='', assignment='', files=[]):
+        self.course = course
+        self.assignment = assignment
+        self.files = files
+
+
+    def setCourse(self, course):
+        self.course = course
+
+    def setAssignment(self, assignment):
+        self.assignment = assignment
+
+    def addFile(self, f):
+        self.files.append(f)
+
+    def submit(self, m):
+        m.submit(self.course, self.assignment, self.files)
+
+
+
+def getParamsFromFile(path):
+    """
+    Get a list of parameters by searching the file.
+
+    param path: Path to the file (string)
+    returns: A list of parameters [course, assignment]
+    """
     f = open(path)
+    fileType = os.path.splitext(path)[1]
+    keyword = langLookup[fileType]
+
     for line in f:
-        if line.startswith(';;;(autosubmit'):
+        if line.startswith(keyword):
             params = line.split(' ', 2)
-            if len(params) != 3: return
+            if len(params) != 3: return False
 
             params.pop(0)
             params[1] = params[1].rstrip(')\n')
@@ -36,18 +71,47 @@ def getParams(path):
     f.close()
     return False
 
-def getFiles(filetype):
-    """Get all the files of the specified type from the cwd"""
+def getFilePaths(filetype):
+    """
+    Get all the files of the specified type from the cwd.
+
+    param filetype: The filetype to search for
+    returns: A list of file paths
+    """
     cwd = os.getcwd()
-    return glob.glob(cwd + '/*.' + filetype)
+    return glob.glob(cwd + '/*' + filetype)
 
-def submitAllFiles():
-    """Submit all valid files in the cwd"""
-    fileList = getFiles('rkt')
-    paramsMap = map(lambda f: (getParams(f), f), fileList)
-    #return paramsMap
-    for fileParams in paramsMap:
-        m.submit(fileParams[0][0], fileParams[0][1], fileParams[1])
+def getAllParams(files):
+    """
+    Get all parameters from the list of files.
 
+    param files: A list of files
+    returns: a list of MarmosetAssignments
+    """
+    marmoProblems = dict()
+    fileList = []
+    for filetype in langLookup:
+        fileList += getFilePaths(filetype)
 
-submitAllFiles()
+    paramsMap = zip(fileList, map(lambda f: getParamsFromFile(f), fileList))
+
+    validFiles = (x for x in paramsMap if x[1] != False)
+    for entry in validFiles:
+        course = entry[1][0]
+        assignment = entry[1][1]
+        fileName = entry[0] # FIXME: Breaks when there are spaces in path
+        id = course + assignment # 'course' + 'assgignment'
+        if id in marmoProblems:
+            # add filename to existing MarmosetAssignment
+            marmoProblems[id].addFile(fileName)
+        else:
+            marmoProblems[id] = MarmosetAssignment(course, assignment)
+            marmoProblems[id].addFile(fileName)
+
+    return marmoProblems.values()
+
+def submitAll(marmosetAssignments, m):
+    for problem in marmosetAssignments:
+        problem.submit(m)
+
+submitAll(getAllParams(2), m)
